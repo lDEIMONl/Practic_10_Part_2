befdc9ccc47e6b9d7e1c3c96673683b09b2008ee
lDEIMONl <73995247+lDEIMONl@users.noreply.github.com>
2021-05-23 18:16:58 +0300

    Initial commit

---
+++ Practic_11/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Practic_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Заполенение массива случаными числами
+            int[,] array = new int[10, 10];
+            Random random = new Random();
+            Console.WriteLine("Начальный массив");
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(1, 10);
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            //Сортировка массива по столбцам
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    for (int k = 0; k < array.GetLength(1); k++)
+                    {
+                        if (array[j, i] >= array[k, i])
+                        {
+                            continue;
+                        }
+                        int temp = array[j, i];
+                        array[j, i] = array[k, i];
+                        array[k, i] = temp;
+                    }
+                }
+                
+            }
+
+            //Вывод отсортированного массива
+            Console.WriteLine("\nОтсортированный массив по столбцам");
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
